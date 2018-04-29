f8d19f73bbc8282567d87bdfd2cb979dfe5f97e3
SekulKamberov <sekul@Sekul.local>
2018-04-29 23:37:55 +0300

    ASP.NET Homeworks
    ASP.NET Homeworks

---
+++ ASP.NET Homeworks /2015/ASP.NET WebServices Cloud/01.Homework.WebApi/02-03.MusicSystem/MusicSystem.Model/Song.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicSystem.Model
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public string  Genre { get; set; }
+
+        public virtual int ArtistId { get; set; }
+
+        public virtual Artist   Artist { get; set; }
+    }
+}
