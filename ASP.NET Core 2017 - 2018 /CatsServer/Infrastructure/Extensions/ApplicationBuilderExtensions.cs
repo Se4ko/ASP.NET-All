b3b2369bf9c5bc7b96692d91f8ac54c585c38990
SekulKamberov <sekul@Sekul.local>
2018-05-21 00:07:03 +0300

    Comments added
    CatsServer

---
+++ ASP.NET Core 2017 - 2018 /CatsServer/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,52 @@
+﻿namespace CatsServer.Infrastructure.Extensions
+{
+    using CatsServer.Handlers;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+    using Middleware;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class ApplicationBuilderExtensions
+    {
+		/* builder.UseMiddleware<>
+		   Adds a middleware type to the application's request pipeline */
+        public static IApplicationBuilder UseDatabaseMigration(this IApplicationBuilder builder)
+            => builder.UseMiddleware<DatabaseMigrationMiddleware>();
+
+        public static IApplicationBuilder UseHtmlContentType(this IApplicationBuilder builder)
+            => builder.UseMiddleware<HtmlContentTypeMiddleware>();
+
+        public static IApplicationBuilder UseRequestHandlers(this IApplicationBuilder builder)
+        {
+            var handlers = Assembly
+                .GetExecutingAssembly()
+				.GetTypes() 
+                .Where(t => t.IsClass && typeof(IHandler).IsAssignableFrom(t))
+                .Select(Activator.CreateInstance)
+                .Cast<IHandler>()
+                .OrderBy(h => h.Order);
+			
+            foreach (var handler in handlers)
+			{/*всеки handler има public Func<HttpContext, bool> Condition => .. тва е ралта 
+               има и делегат RequestDelegate RequestHandler => в който се изпълняват нещата..                                                               */
+                builder.MapWhen(handler.Condition, app =>//на конкретния ралт
+                {
+					app.Run(handler.RequestHandler);// app.Run(..) изпълни каквото има и приключвай
+                });
+            }
+
+            return builder;
+        }
+
+        public static void UseNotFoundHandler(this IApplicationBuilder builder)
+        {
+            builder.Run(async (context) =>
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("404 Page Was Not Found!");
+            });
+        }
+    }
+}
---
+++ ASP.NET Core 2017 - 2018 /CatsServer/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace CatsServer
+{
+    using Data;
+    using Infrastructure.Extensions;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+            => services.AddDbContext<CatsDbContext>(options =>
+                options.UseSqlServer(AppSettings.DatabaseConnectionString));
+
+        public void Configure(IApplicationBuilder app)
+            => app
+                .UseDatabaseMigration()
+                .UseStaticFiles()
+                .UseHtmlContentType()
+                .UseRequestHandlers()
+                .UseNotFoundHandler();
+    }
+}
+
+/* 
+Правиме ApplicationBuilderExtensions.cs и 
+вътре са всички методи app.UseDatabaseMigration() .., в тях на 
+IApplicationBuilder builder-а закачаме Middlewares
+(които са класове в Middleware папка)
+
+UseDatabaseMigration() метода, закача DatabaseMigrationMiddleware.cs class:
+IApplicationBuilder builder.UseMiddleware<DatabaseMigrationMiddleware>();
+
+UseHtmlContentType() метода, закача HtmlContentTypeMiddleware.cs class:
+IApplicationBuilder builder.UseMiddleware<HtmlContentTypeMiddleware>();
+
+Може и там да си напишем Middleware-а, както UseRequestHandlers,
+който map-ва за IApplicationBuilder builder всички 
+
+*/
